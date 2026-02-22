04f78d5cb05c5fd3d37a7c87567be5847b0b5a1a
Daniel Philipov <batkodanny@gmail.com>
2026-02-22 16:15:55 -0600

    vibed a website

---
+++ src/assets/project-icons.ts
@@ -0,0 +1,43 @@
+import astroIcon from "@/assets/catppuccin_icons/astro.svg?url";
+import bashIcon from "@/assets/catppuccin_icons/bash.svg?url";
+import cIcon from "@/assets/catppuccin_icons/c.svg?url";
+import cppIcon from "@/assets/catppuccin_icons/cpp.svg?url";
+import cudaIcon from "@/assets/catppuccin_icons/cuda.svg?url";
+import dockerIcon from "@/assets/catppuccin_icons/docker.svg?url";
+import gitIcon from "@/assets/catppuccin_icons/git.svg?url";
+import godotIcon from "@/assets/catppuccin_icons/godot.svg?url";
+import javaIcon from "@/assets/catppuccin_icons/java.svg?url";
+import javascriptIcon from "@/assets/catppuccin_icons/javascript.svg?url";
+import jupyterIcon from "@/assets/catppuccin_icons/jupyter.svg?url";
+import latexIcon from "@/assets/catppuccin_icons/latex.svg?url";
+import macosIcon from "@/assets/catppuccin_icons/macos.svg?url";
+import nixIcon from "@/assets/catppuccin_icons/nix.svg?url";
+import ocamlIcon from "@/assets/catppuccin_icons/ocaml.svg?url";
+import pythonIcon from "@/assets/catppuccin_icons/python.svg?url";
+import reactIcon from "@/assets/catppuccin_icons/javascript-react.svg?url";
+import rustIcon from "@/assets/catppuccin_icons/rust-alt.svg?url";
+import unityIcon from "@/assets/catppuccin_icons/unity.svg?url";
+import zigIcon from "@/assets/catppuccin_icons/zig.svg?url";
+
+export {
+  astroIcon,
+  bashIcon,
+  cIcon,
+  cppIcon,
+  cudaIcon,
+  dockerIcon,
+  gitIcon,
+  godotIcon,
+  javaIcon,
+  javascriptIcon,
+  jupyterIcon,
+  latexIcon,
+  macosIcon,
+  nixIcon,
+  ocamlIcon,
+  pythonIcon,
+  reactIcon,
+  rustIcon,
+  unityIcon,
+  zigIcon,
+};
