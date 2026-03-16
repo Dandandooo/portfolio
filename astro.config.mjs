810e1febd8eb541d4b27ffcb582a2f8fa414da76
Daniel Philipov <batkodanny@gmail.com>
2026-03-16 16:00:30 -0400

    fix: set base to /portfolio for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://dandandooo.github.io/",
-  base: "/",
+  base: "/portfolio",
   integrations: [icon()]
 });
