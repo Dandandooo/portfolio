0f4ae94579b9af42e1a750454f3b7b6082ce7a1b
Daniel Philipov <batkodanny@gmail.com>
2026-03-16 16:05:10 -0400

    deploy errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://dandandooo.github.io/",
-  base: "/portfolio",
+  base: "/",
   integrations: [icon()]
 });
